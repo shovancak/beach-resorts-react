071b60a2b2d036a689fcf8eacf886aa10281251c
shovancak <s.hovancak@gmail.com>
2020-05-24 21:53:58 +0200

    Single Room Component Slug Error Handling

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -8,12 +8,49 @@ import { RoomContext } from "../Context";
 export default class SingleRoom extends Component {
   constructor(props) {
     super(props);
-    console.log(this.props);
+    this.state = {
+      slug: this.props.match.params.slug,
+      defaultBcg,
+    };
   }
+  static contextType = RoomContext;
 
-  componentDidMount() {}
+  // componentDidMount() {}
 
   render() {
-    return <div>Hello from SINGLEROOM PAGE</div>;
+    const { getRoom } = this.context;
+    const room = getRoom(this.state.slug);
+    console.log(room);
+    if (!room) {
+      return (
+        <div className="error">
+          <h3>No such room could be found...</h3>
+          <Link to="/rooms" className="btn-primary">
+            Back to rooms
+          </Link>
+        </div>
+      );
+    }
+    const {
+      name,
+      description,
+      capacity,
+      size,
+      price,
+      extras,
+      breakfast,
+      pets,
+      images,
+    } = room;
+
+    return (
+      <Hero hero="roomsHero">
+        <Banner title={`${name} room`}>
+          <Link to="/rooms" className="btn-primary">
+            Back to rooms
+          </Link>
+        </Banner>
+      </Hero>
+    );
   }
 }
