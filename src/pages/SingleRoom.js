361af1cb76660dd050118ffaf90df8a415a268f4
shovancak <s.hovancak@gmail.com>
2020-05-25 21:48:11 +0200

    Adding informations and extras for every SingleRoom component

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -59,6 +59,31 @@ export default class SingleRoom extends Component {
               return <img key={index} src={image} alt={name}></img>;
             })}
           </div>
+          <div className="single-room-info">
+            <article className="desc">
+              <h3>details</h3>
+              <p>{description}</p>
+            </article>
+            <article className="info">
+              <h3>info</h3>
+              <h6>price : ${price}</h6>
+              <h6>size: {size} SQFT</h6>
+              <h6>
+                max capacity :{" "}
+                {capacity > 1 ? `${capacity} People` : `${capacity} Person`}
+              </h6>
+              <h6>{pets ? "Pets Allowed" : "No Pets Allowed"}</h6>
+              <h6>{breakfast && "Free Breakfast Included"}</h6>
+            </article>
+          </div>
+        </section>
+        <section className="room-extras">
+          <h6>extras</h6>
+          <ul className="extras">
+            {extras.map((item, index) => {
+              return <li key={index}>- {item}</li>;
+            })}
+          </ul>
         </section>
       </React.Fragment>
     );
